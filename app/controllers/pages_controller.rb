e4c26b8e4f9b609e60a4ede96ad45c51a75d643f
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-03-15 00:42:36 +0100

    add PagesController#impressum++, ...

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  # skip_authorization_verification
+
+  def impressum
+  end
+
+  def privacy
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'seminars#index'
+
+  get 'impressum', to: 'pages#impressum'
+  get 'privacy',   to: 'pages#privacy'
+  get 'contact',   to: 'pages#contact'
+
   resources :seminars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
