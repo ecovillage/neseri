3f71099fb8a99d1b7af5b0eb1eccf5367dbaac68
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-03-14 22:34:53 +0100

    disable authorization verification at every call in prod. mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  verify_authorized
+  verify_authorized if !Rails.env.production?
+
   rescue_from ActionPolicy::Unauthorized do |exception|
     redirect_back fallback_location: root_path,
       flash: {alert: :not_authorized }#, status: :forbidden
