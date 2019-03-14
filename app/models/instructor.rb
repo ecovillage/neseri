7d49c0a19bcf67bf84937eb99914cbab38dedb06
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-03-14 17:09:29 +0100

    gem 'annotate' && annotate

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: instructors
+#
+#  id         :integer          not null, primary key
+#  firstname  :string
+#  lastname   :string
+#  address    :string
+#  email      :string
+#  fax        :string
+#  phone      :string
+#  mobile     :string
+#  homepage   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Instructor < ApplicationRecord
 end
--- app/models/seminar.rb
+++ app/models/seminar.rb
@@ -1,3 +1,33 @@
+# == Schema Information
+#
+# Table name: seminars
+#
+#  id                          :integer          not null, primary key
+#  title                       :string
+#  subtitle                    :string
+#  description                 :text
+#  attendees_minimum           :integer
+#  attendees_maximum           :integer
+#  attendees_preconditions     :string
+#  please_bring                :string
+#  start_date                  :datetime
+#  end_date                    :datetime
+#  cancellation_time           :integer          default(7)
+#  cancellation_reason         :string
+#  room_material               :string
+#  room_extras                 :string
+#  royalty_participant         :decimal(, )
+#  royalty_participant_reduced :decimal(, )
+#  material_cost               :decimal(, )
+#  honorar                     :decimal(, )
+#  kind                        :string           default("user")
+#  uuid                        :string
+#  locked                      :boolean          default(FALSE)
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  creator_id                  :integer
+#
+
 class Seminar < ApplicationRecord
   belongs_to :creator, class_name: "User"
 
