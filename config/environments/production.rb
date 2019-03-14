267c8d59d4613651678d362351dbbb45c0df64fa
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-03-14 23:44:54 +0100

    split smtp address and domain (for HELO)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,9 +70,9 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV['MAILER_DOMAIN'],
+    address:              ENV['SMTP_SERVER'],
     port:                 ENV['SMTP_PORT'],
-    domain:               ENV['MAILER_DOMAIN'],
+    domain:               ENV['SMTP_DOMAIN'],
     user_name:            ENV['SMTP_USER'],
     password:             ENV['SMTP_PWD'],
     authentication:       'plain',
