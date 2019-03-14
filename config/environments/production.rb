ce0ba2a910707944bdafb8de2d244dd7d2542685
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-03-14 22:50:05 +0100

    production: smtp settings via ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,17 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['MAILER_DOMAIN'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['MAILER_DOMAIN'],
+    user_name:            ENV['SMTP_USER'],
+    password:             ENV['SMTP_PWD'],
+    authentication:       'plain',
+    enable_starttls_auto: true }
+  
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
