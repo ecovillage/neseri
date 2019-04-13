8415b1b7a74b5caeb53637e5df79157fe927114a
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-04-13 18:09:13 +0200

    add Admin#email view.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   get 'about',     to: 'pages#about'
 
   resources :seminars
+  namespace :admin do
+    resources :emails, only: [:index]
+  end
+
   resource :instructor, only: [:show, :create, :edit, :update]
   resolve('Instructor') { :instructor }
 
