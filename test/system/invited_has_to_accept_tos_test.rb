0ab0e695f2f1068755ad4a922af558a616d6874e
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-04-27 10:18:20 +0200

    add test stub for invitation tos accept

---
+++ test/system/invited_has_to_accept_tos_test.rb
@@ -0,0 +1,48 @@
+require "application_system_test_case"
+
+class InvitedHasToAcceptTosTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  test "an invited has to accept tos to create user" do
+    sign_in users(:admin)
+
+    # User will be created on invitation
+    user_count = User.count
+    refute User.find_by(email: 'jones@neseri.tu')
+    assert_equal 0, ActionMailer::Base.deliveries.count
+
+    # Add invited user (add instructor)
+    visit edit_seminar_path(seminars(:one))
+    assert_selector "h1", text: "Vorschlag bearbeiten"
+                       # 'Referent/In hinzufügen'
+    click_link_or_button 'Referent/In hinzufügen'
+    fill_in 'E-Mail-Adresse', with: 'jones@neseri.tu'
+    click_link_or_button 'Seminarvorschlag speichern'
+    #assert_response :success
+
+    # a new user was created.
+    assert (user_count + 1) == User.count
+    jones = User.find_by(email: 'jones@neseri.tu')
+    assert jones
+
+    # log out as admin
+    sign_out users(:admin)
+
+
+    # Follow invitation link as jones.
+    # click the link but you have to accept tos/terms.
+    visit 'users/invitation/accept?invitation_token=hns-dvpnWby-TsLD-AnK'
+    assert_selector '.notification', text: 'Der Einladungslink ist ungültig'
+
+    # Mail has been sent
+    mail = ActionMailer::Base.deliveries.last
+    mail_body = mail.body.parts[0].decoded
+    # e.g. http://localhost/users/invitation/accept?invitation_token=yySnSWtD8ERZWDPNkW6s\r
+    /^http:\/\/localhost\/(?<invitation_path>.*)/ =~ mail_body
+
+    visit invitation_path
+
+    refute_selector '.notification', text: 'Der Einladungslink ist ungültig'
+    fill_in 'Passwort', with: 'jones@neseri.tu'
+  end
+end
