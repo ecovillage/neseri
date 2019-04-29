21fb58afc2d822b2ce782c9e0701d444516c6f83
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-04-29 11:38:23 +0200

    set after_sign_in_path to seminars_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :tos_agreement])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:tos_agreement])
   end
+
+  def after_sign_in_path_for(resource)
+    seminars_path(current_user)
+  end
 end
