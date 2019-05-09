d584d5c5b64ba4ff88f99a4a64096bdafd58fa4d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-05-09 22:41:27 +0200

    add production_url setting

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -3,19 +3,20 @@ class Settings
   include ActiveModel::Attributes
 
   attribute :legacy_uri
+  attribute :production_url
 
   def save
     Setting.find_or_initialize_by(key: 'legacy_uri').update!(value: legacy_uri)
+    Setting.find_or_initialize_by(key: 'production_url').update!(value: production_url)
   end
 
-  def update
-    Setting.find_or_initialize_by(key: 'legacy_uri').update(value: legacy_uri).save
-  end
 
   def self.load
     legacy_uri = Setting.find_or_create_by(key: 'legacy_uri').value
+    production_url = Setting.find_or_create_by(key: 'production_url').value
     Settings.new(
-      legacy_uri: legacy_uri
+      legacy_uri: legacy_uri,
+      production_url: production_url
     )
   end
 end
