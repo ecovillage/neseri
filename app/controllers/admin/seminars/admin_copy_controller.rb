c8d336604cfb2442317e3d64e4c8022df984d3eb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-05-09 18:11:47 +0200

    fix admin copy controller

--- app/controllers/admin/seminars/admin_copy_controller.rb
+++ app/controllers/admin/seminars/admin_copy_controller.rb
@@ -6,17 +6,18 @@ class Admin::Seminars::AdminCopyController < NeseriController
     authorize! @user_seminar, with: AdminSeminarPolicy
 
     if @user_seminar.admin_seminar
-      helpers.add_flash t(:has_already_admin_seminar)
-      redirect_to admin_seminar_path(@user_seminar.admin_seminar)
+      helpers.add_flash t("admin.seminar.admin_copy.has_already")
+      @admin_seminar = @user_seminar.admin_seminar
     elsif @user_seminar.is_admin_seminar?
-      helpers.add_flash t(:is_already_admin_seminar)
-      redirect_to admin_seminar_path(@user_seminar)
+      helpers.add_flash t("admin.seminar.admin_copy.is_already")
+      @admin_seminar = @user_seminar
     else
       operation = AdminSeminarCloner.call(@user_seminar)
       @admin_seminar = operation.to_record
       @admin_seminar.save!
-      helpers.add_flash notice: t(:admin_copy_created)
-      redirect_to admin_seminar_path(@admin_seminar)
+      helpers.add_flash notice: t("admin.seminar.admin_copy.created")
     end
+
+    redirect_to edit_admin_admin_seminar_path(@admin_seminar)
   end
 end
