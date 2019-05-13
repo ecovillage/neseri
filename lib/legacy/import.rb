77f1abd3e2bcf1246422fc4ccbb14b89ef05d18b
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-05-13 13:56:34 +0200

    import admin seminars

--- lib/legacy/import.rb
+++ lib/legacy/import.rb
@@ -3,16 +3,22 @@ module Legacy
     def self.seminar_from_hash seminar_data
       instructors = seminar_data["instructors"]
       # TODO bring back timetype and room, and others
-      # TODO take admin_seminar and deal with it
 
       ["instructors", "old_id", "timetype", "room", "payment_royalties", "regionalplatz", "time_signature", "active", "published", "instructor_id", "origin_id"].each do |key|
         seminar_data.delete(key)
+      end
+
+      admin_seminar_data = seminar_data.delete('admin_seminar')
+      if admin_seminar_data
+        admin_seminar = seminar_from_hash admin_seminar_data
+        # We know this can't loop.
       end
 
       start_time = seminar_data.delete "start_time"
       end_time   = seminar_data.delete "end_time"
 
       seminar = Seminar.new seminar_data
+      seminar.locked = true
 
       start_hour, start_min = start_time.scan(/\d{2}/)
       end_hour,   end_min   = end_time.scan(/\d{2}/)
@@ -25,6 +31,12 @@ module Legacy
         comment = i["room"]
         seminar.seminar_instructors.build(email: i["email"], user: User.find_by(email: i["email"]), qualification: i["qualification"]) if i["email"]
       end
+
+      if admin_seminar
+        seminar.admin_seminar = admin_seminar
+      end
+
+      seminar
     end
   end
 end
--- lib/tasks/import_json_from_legacy_db.rake
+++ lib/tasks/import_json_from_legacy_db.rake
@@ -48,28 +48,7 @@ namespace :neseri do
     end
 
     data["seminars"].each do |seminar_data|
-      instructors = seminar_data["instructors"]
-      # TODO bring back timetype and room, and others
-      # TODO take admin_seminar and deal with it
-      ["instructors", "old_id", "timetype", "room", "payment_royalties", "regionalplatz", "time_signature", "active", "published", "instructor_id", "origin_id"].each do |key|
-        seminar_data.delete(key)
-      end
-      start_time = seminar_data.delete "start_time"
-      end_time   = seminar_data.delete "end_time"
-
-      seminar = Seminar.new seminar_data
-
-      start_hour, start_min = start_time.scan(/\d{2}/)
-      end_hour,   end_min   = end_time.scan(/\d{2}/)
-      seminar.start_date = seminar.start_date.change({hour: start_hour,
-        min:  start_min})
-      seminar.end_date   = seminar.end_date.change({hour: end_hour,
-        min:  end_min})
-
-      [*instructors].each do |i|
-        comment = i["room"]
-        seminar.seminar_instructors.build(email: i["email"], user: User.find_by(email: i["email"]), qualification: i["qualification"]) if i["email"]
-      end
+      seminar = Legacy::Import.seminar_from_hash seminar_data
       seminar.save
     end
 
