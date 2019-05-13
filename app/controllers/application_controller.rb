f0109b5bfc5df692471bac9a4dc1440bc15c77bc
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-05-13 11:51:56 +0200

    i18n for impersonation (pretender gem)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_devise_parameters, if: :devise_controller?
+  impersonates :user
 
   def configure_permitted_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :tos_agreement])
