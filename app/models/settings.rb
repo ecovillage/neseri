e049e297e38bb7d2b1e6f2ae4c26ec57650c7c20
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-05-08 09:19:11 +0200

    another todo if settings are ever needed again

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -10,6 +10,10 @@ class Settings
     Setting.find_or_initialize_by(key: 'legacy_web_url').update!(value: legacy_web_url)
   end
 
+  # TODO if ever needed again, this helper would make saving/setting easier
+  #def self.set key, value
+  #  Setting.find_or_initialize_by(key: key).update(value: value).save
+  #end
 
   def self.load
     legacy_db_uri = Setting.find_or_create_by(key: 'legacy_db_uri').value
