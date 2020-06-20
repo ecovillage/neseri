5d09c8918c937e0282e0eafb74b2682d7c42dd8d
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-06-20 14:00:07 +0200

    prevent cloning of admin copies

--- app/controllers/seminars/clone_controller.rb
+++ app/controllers/seminars/clone_controller.rb
@@ -5,11 +5,16 @@ class Seminars::CloneController < NeseriController
     @old_seminar = Seminar.find(params[:seminar_id])
     authorize! @old_seminar, with: SeminarPolicy
 
-    operation = SeminarCloner.call(@old_seminar, current_user: current_user)
-    @seminar  = operation.to_record
+    if @old_seminar.is_admin_seminar?
+      flash[:error] = t(:can_not_copy_admin_seminar)
+      redirect_to @old_seminar
+    else
+      operation = SeminarCloner.call(@old_seminar, current_user: current_user)
+      @seminar  = operation.to_record
 
-    flash[:notice] = t(:copy_created)
+      flash[:notice] = t(:copy_created)
 
-    render 'seminars/new'
+      render 'seminars/new'
+    end
   end
 end
