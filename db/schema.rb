13fb388e744fe7e363962eefbbf8da8b1d3b2097
Martin Eismann <softwerk@eismann.cc>
2026-05-13 12:01:55 +0200

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -2,112 +2,126 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_181044) do
-
+ActiveRecord::Schema[8.1].define(version: 2020_06_23_095624) do
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
     t.integer "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.integer "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "ahoy_messages", force: :cascade do |t|
-    t.string "user_type"
-    t.integer "user_id"
-    t.text "to"
-    t.string "mailer"
-    t.text "subject"
-    t.datetime "sent_at"
     t.text "content"
+    t.string "mailer"
+    t.datetime "sent_at"
+    t.text "subject"
+    t.text "to"
+    t.integer "user_id"
+    t.string "user_type"
     t.index ["user_type", "user_id"], name: "index_ahoy_messages_on_user_type_and_user_id"
   end
 
+  create_table "publication_user_mappings", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "user_id"
+    t.string "uuid"
+    t.index ["user_id"], name: "index_publication_user_mappings_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "kind"
     t.boolean "active"
+    t.datetime "created_at", null: false
+    t.text "kind"
+    t.string "name"
+    t.datetime "updated_at", null: false
     t.index ["kind"], name: "index_rooms_on_kind"
   end
 
   create_table "seminar_instructors", force: :cascade do |t|
-    t.integer "seminar_id"
-    t.integer "user_id"
-    t.text "qualification"
-    t.boolean "main_contact"
     t.string "accommodation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email"
+    t.string "address"
     t.string "comment"
     t.boolean "contactable", default: false
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "fax"
+    t.string "firstname"
+    t.string "homepage"
+    t.string "lastname"
+    t.boolean "main_contact"
+    t.string "mobile"
+    t.string "phone"
+    t.text "qualification"
+    t.integer "seminar_id"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["email"], name: "index_seminar_instructors_on_email"
     t.index ["seminar_id"], name: "index_seminar_instructors_on_seminar_id"
     t.index ["user_id"], name: "index_seminar_instructors_on_user_id"
   end
 
   create_table "seminar_kinds", force: :cascade do |t|
-    t.string "name"
     t.boolean "active", default: true
-    t.text "description"
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "seminars", force: :cascade do |t|
-    t.string "title"
-    t.string "subtitle"
-    t.text "description"
-    t.integer "attendees_minimum"
+    t.string "accommodation"
+    t.boolean "active", default: true
+    t.text "alternative_dates"
     t.integer "attendees_maximum"
+    t.integer "attendees_minimum"
     t.string "attendees_preconditions"
-    t.string "please_bring"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.integer "cancellation_time", default: 7
     t.string "cancellation_reason"
-    t.string "room_material"
+    t.integer "cancellation_time", default: 7
+    t.datetime "created_at", null: false
+    t.integer "creator_id"
+    t.text "description"
+    t.datetime "end_date"
+    t.string "kind", default: "user"
+    t.boolean "locked", default: false
+    t.decimal "material_cost"
+    t.text "other_extras"
+    t.string "please_bring"
+    t.text "room_comment"
     t.string "room_extras"
+    t.string "room_material"
+    t.integer "room_wish_id"
     t.decimal "royalty_participant"
     t.decimal "royalty_participant_reduced"
-    t.decimal "material_cost"
-    t.string "kind", default: "user"
-    t.string "uuid"
-    t.boolean "locked", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "creator_id"
     t.integer "seminar_kind_id"
-    t.string "accommodation"
+    t.datetime "start_date"
+    t.string "subtitle"
+    t.string "title"
+    t.datetime "updated_at", null: false
     t.integer "user_seminar_id"
-    t.text "alternative_dates"
-    t.text "other_extras"
-    t.integer "room_wish_id"
-    t.boolean "active", default: true
-    t.text "room_comment"
+    t.string "uuid"
     t.index ["creator_id"], name: "index_seminars_on_creator_id"
     t.index ["room_wish_id"], name: "index_seminars_on_room_wish_id"
     t.index ["seminar_kind_id"], name: "index_seminars_on_seminar_kind_id"
@@ -115,42 +129,42 @@ ActiveRecord::Schema.define(version: 2019_05_10_181044) do
   end
 
   create_table "settings", force: :cascade do |t|
-    t.string "key"
-    t.string "value"
     t.datetime "created_at", null: false
+    t.string "key"
     t.datetime "updated_at", null: false
+    t.string "value"
     t.index ["key"], name: "index_settings_on_key", unique: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "address"
+    t.boolean "admin", default: false
+    t.datetime "confirmation_sent_at"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
-    t.string "firstname"
-    t.string "lastname"
-    t.string "address"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "fax"
-    t.string "phone"
-    t.string "mobile"
+    t.string "firstname"
     t.string "homepage"
-    t.date "tos_accepted_at"
-    t.string "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
+    t.datetime "invitation_created_at"
     t.integer "invitation_limit"
-    t.string "invited_by_type"
-    t.integer "invited_by_id"
+    t.datetime "invitation_sent_at"
+    t.string "invitation_token"
     t.integer "invitations_count", default: 0
+    t.integer "invited_by_id"
+    t.string "invited_by_type"
+    t.string "lastname"
+    t.string "mobile"
+    t.string "phone"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.date "tos_accepted_at"
+    t.string "unconfirmed_email"
+    t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
@@ -160,4 +174,6 @@ ActiveRecord::Schema.define(version: 2019_05_10_181044) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "publication_user_mappings", "users"
 end
