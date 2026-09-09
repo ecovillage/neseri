5713371f80c6ff03ba8432fb6c809f0dac5dab36
Martin Eismann <softwerk@eismann.cc>
2026-09-09 08:56:13 +0200

    Revert "Request more user info on registration"
    Is not used, since this info is requested (again) on instructor setup.
    
    This reverts commit 5de66c52314701f328e55c3d80fb63d00d98e89c.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   impersonates :user
 
   def configure_permitted_devise_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :tos_agreement,
-      :firstname, :lastname, :address, :phone, :mobile, :homepage])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :tos_agreement])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:tos_agreement])
   end
 
